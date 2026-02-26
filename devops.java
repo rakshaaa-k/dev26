97b237327f02758885591e036c83ce8fa3387dfe
raksha~ <raksharithu171@gmail.com>
2026-02-26 13:16:16 +0530

    Use fixed values for num1 and num2 in devops
    Replaced user input with fixed values for num1 and num2.

--- devops.java
+++ devops.java
@@ -2,18 +2,12 @@ import java.util.Scanner;
 
 public class devops {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
+        int num1 = 10;
+        int num2 = 20;
 
         int sum = num1 + num2;
 
         System.out.println("Sum = " + sum);
-
-        scanner.close();
     }
 }
