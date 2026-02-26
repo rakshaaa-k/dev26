71694c1bea6499151049c301bf99e1a56d7fc501
raksha~ <raksharithu171@gmail.com>
2026-02-26 13:14:12 +0530

    Refactor devops.java to sum two numbers
    Removed random number guessing game and added functionality to sum two user inputs.

--- devops.java
+++ devops.java
@@ -1,31 +1,18 @@
 import java.util.Scanner;
-import java.util.Random;
 
 public class devops {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
-        int secretNumber = random.nextInt(50) + 1; // 1 to 50
-        int guess;
-        int attempts = 0;
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
 
-        System.out.println("Guess a number between 1 and 50:");
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
 
-        do {
-            System.out.print("Enter your guess: ");
-            guess = scanner.nextInt();
-            attempts++;
+        int sum = num1 + num2;
 
-            if (guess > secretNumber) {
-                System.out.println("Too high!");
-            } else if (guess < secretNumber) {
-                System.out.println("Too low!");
-            } else {
-                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
-            }
-
-        } while (guess != secretNumber);
+        System.out.println("Sum = " + sum);
 
         scanner.close();
     }
